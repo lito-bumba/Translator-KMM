3b53e0968f051fb818cdc41f5ff922053f804f07
Lito Bumba <litocdbumba@gmail.com>
2023-12-23 20:19:38 -0300

    Setup iOS App

--- shared/src/androidMain/kotlin/com/litobumba/translator_kmm/Platform.android.kt
+++
@@ -1,7 +0,0 @@
-package com.litobumba.translator_kmm
-
-class AndroidPlatform : Platform {
-    override val name: String = "Android ${android.os.Build.VERSION.SDK_INT}"
-}
-
-actual fun getPlatform(): Platform = AndroidPlatform()
--- shared/src/iosMain/kotlin/com/litobumba/translator_kmm/Platform.ios.kt
+++ shared/src/iosMain/kotlin/com/litobumba/translator_kmm/Platform.ios.kt
@@ -2,8 +2,6 @@ package com.litobumba.translator_kmm
 
 import platform.UIKit.UIDevice
 
-class IOSPlatform: Platform {
-    override val name: String = UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
+class IOSPlatform {
+    val name: String = UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
 }
-
-actual fun getPlatform(): Platform = IOSPlatform()
